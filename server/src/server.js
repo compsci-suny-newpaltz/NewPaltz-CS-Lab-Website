6c7fd1eddffe15441151f2adc00ae5707ae4a840
MollyTSlater <mollytslater@gmail.com>
2025-11-15 13:32:49 -0500

    missing slash

--- server/src/server.js
+++ server/src/server.js
@@ -32,7 +32,7 @@ app.use("/tech-blog", techBlogRoutes);
 app.use("/student-highlights", studentHighlightRoutes);
 app.use("/sd-forms", sdFormRoutes);
 app.use("/student", student2Routes);
-app.use("profile", profileRoutes);
+app.use("/profile", profileRoutes);
 //app.use("/scripts", adminProxy); 
 
 // Uncomment the following lines to enable these routes when needed
